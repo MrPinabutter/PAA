24210fd20796910b0d972e744be230d70a62858f
Vitor <vitorafael.r.c@gmail.com>
2021-05-20 19:05:37 -0300

    heapsort

--- heapsort.py
+++ heapsort.py
@@ -1,7 +1,36 @@
 import time
 
-def heapsort():
-  pass
+class Heap:
+    def heapify(self, a = [], n = 0, i = 0):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+
+        if l < n and a[l] > a [largest]:
+            largest = l
+
+        if r < n and a[r] > a[largest]:
+            largest = r
+
+        if largest != i:
+            a[i], a[largest] = a[largest], a[i]
+
+            self.heapify(a, n, largest)
+
+    def sort(self, a):
+        n = len(a)
+
+        for i in range(int(n/2 - 1), -1, -1):
+            self.heapify(a, n, i)
+
+        for i in range(n - 1, -1, -1):
+            temp = a[0]
+            a[0] = a[i]
+            a[i] = temp
+
+            self.heapify(a, i, 0)
+
+
 
 arr = []
 while(True):
@@ -12,7 +41,8 @@ while(True):
     break 
 
 ini = time.time()
-heapsort(arr, 0, len(arr)-1)
+heap = Heap()
+heap.sort(a = arr)
 fim = time.time()
 print("Tempo de execução:", fim-ini)
 print(arr)
